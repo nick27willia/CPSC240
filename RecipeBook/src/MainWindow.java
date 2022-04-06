6ce9053ecc82200eddd60c8b065414ff2013eee1
Nate <nate@2bttns.com>
2022-04-05 20:56:36 -0400

    Add buildRecipeListPanel function

--- RecipeBook/src/MainWindow.java
+++ RecipeBook/src/MainWindow.java
@@ -21,13 +21,14 @@ public class MainWindow {
         this.baseLayoutPanel = new JPanel();
         BoxLayout baseLayout = new BoxLayout(this.baseLayoutPanel, BoxLayout.X_AXIS);
         this.baseLayoutPanel.setLayout(baseLayout);
-        this.baseLayoutPanel.setBackground(Color.decode("#DDE0FB"));
 
         this.buildRecipeListPanel();
+        this.buildRecipeInfoPanel();
 
         this.baseFrame.getContentPane().add(this.baseLayoutPanel);
         this.baseFrame.setPreferredSize(new Dimension(1000, 600));
         this.baseFrame.pack();
+        this.baseFrame.setResizable(false);
         this.baseFrame.setVisible(true);
     }
 
@@ -35,7 +36,7 @@ public class MainWindow {
     public void buildRecipeListPanel(){
         JPanel recipeListPanel = new JPanel();
         recipeListPanel.setBackground(Color.decode("#98A2FF"));
-        recipeListPanel.setBorder(new EmptyBorder(12, 12, 12, 12));
+        recipeListPanel.setBorder(new EmptyBorder(16, 16, 16, 16));
         Dimension recipeListPanelDimensions = new Dimension(300, 600);
         recipeListPanel.setPreferredSize(recipeListPanelDimensions);
         recipeListPanel.setMaximumSize(recipeListPanelDimensions);
@@ -45,9 +46,11 @@ public class MainWindow {
         recipeListPanel.setLayout(recipeListPanelLayout);
 
         JLabel header = new JLabel("Recipes");
-        header.setFont(new Font("Dialog", Font.BOLD, 18));
+        header.setFont(new Font("Dialog", Font.BOLD, 20));
         recipeListPanel.add(header);
 
+        // Aligns button at bottom
+        recipeListPanel.add(Box.createVerticalGlue());
         JButton addNewRecipeButton = new Button("Add New Recipe");
         addNewRecipeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
@@ -57,5 +60,24 @@ public class MainWindow {
         recipeListPanel.add(addNewRecipeButton);
 
         this.baseLayoutPanel.add(recipeListPanel);
+    }
+    // This creates the recipe info panel
+    public void buildRecipeInfoPanel(){
+        JPanel recipeInfoPanel = new JPanel();
+        recipeInfoPanel.setBackground(Color.decode("#DDE0FB"));
+        recipeInfoPanel.setBorder(new EmptyBorder(12, 12, 12, 12));
+        Dimension recipeInfoPanelDimensions = new Dimension(700, 600);
+        recipeInfoPanel.setPreferredSize(recipeInfoPanelDimensions);
+        recipeInfoPanel.setMaximumSize(recipeInfoPanelDimensions);
+
+        // Sets a vertical layout
+        BoxLayout recipeInfoPanelLayout = new BoxLayout(recipeInfoPanel, BoxLayout.Y_AXIS);
+        recipeInfoPanel.setLayout(recipeInfoPanelLayout);
+
+        JLabel header = new JLabel("Chicken and Rice");
+        header.setFont(new Font("Dialog", Font.BOLD, 24));
+        recipeInfoPanel.add(header);
+
+        this.baseLayoutPanel.add(recipeInfoPanel);
     }
 }
