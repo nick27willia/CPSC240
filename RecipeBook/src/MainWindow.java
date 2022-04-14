3b7c8f7e478c9e82b2b2d6bb5909b0721cffa593
Nate <nate@2bttns.com>
2022-04-14 01:12:24 -0400

    Finish interface design

--- RecipeBook/src/MainWindow.java
+++ RecipeBook/src/MainWindow.java
@@ -26,7 +26,7 @@ public class MainWindow {
         this.buildRecipeInfoPanel();
 
         this.baseFrame.getContentPane().add(this.baseLayoutPanel);
-        this.baseFrame.setPreferredSize(new Dimension(1000, 600));
+        this.baseFrame.setPreferredSize(new Dimension(800, 600));
         this.baseFrame.pack();
         this.baseFrame.setResizable(false);
         this.baseFrame.setVisible(true);
@@ -48,10 +48,41 @@ public class MainWindow {
         JLabel header = new JLabel("Recipes");
         header.setFont(new Font("Dialog", Font.BOLD, 20));
         recipeListPanel.add(header);
+        recipeListPanel.add(Box.createRigidArea(new Dimension(0, 16)));
+
+        JLabel sortLabel = new JLabel("Sort");
+        sortLabel.setFont(new Font("Dialog", Font.PLAIN, 14));
+        recipeListPanel.add(sortLabel);
+        recipeListPanel.add(Box.createRigidArea(new Dimension(0, 4)));
+
+        // Creates the sorting options combobox
+        String[] sortOptions = new String[]{"Alphabetically, A-Z"};
+        JComboBox<String> sortOptionsList = new JComboBox<>(sortOptions);
+        Dimension sortOptionsListDimensions = new Dimension(300, 36);
+        sortOptionsList.setPreferredSize(sortOptionsListDimensions);
+        sortOptionsList.setMaximumSize(sortOptionsListDimensions);
+        sortOptionsList.setAlignmentX(0);
+        recipeListPanel.add(sortOptionsList);
+        recipeListPanel.add(Box.createRigidArea(new Dimension(0, 4)));
+
+        // Creates the array for the recipes list component
+        DefaultListModel<String> recipesModel = new DefaultListModel<>();
+        recipesModel.addElement("Bacon Egg & Cheese");
+        recipesModel.addElement("Spaghetti");
+        recipesModel.addElement("BBQ Sandwich");
+
+        // Creates the recipes list component
+        JList<String> recipesList = new JList<>(recipesModel);
+        recipesList.setAlignmentX(0);
+        Dimension recipesListDimensions = new Dimension(300, 250);
+        recipesList.setPreferredSize(recipesListDimensions);
+        recipesList.setMaximumSize(recipesListDimensions);
+        recipeListPanel.add(recipesList);
 
         // Aligns button at bottom
         recipeListPanel.add(Box.createVerticalGlue());
         JButton addNewRecipeButton = new Button("Add New Recipe");
+        // Opens the popup window on click
         addNewRecipeButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
                 popupWindow.show(true);
@@ -66,7 +97,7 @@ public class MainWindow {
         JPanel recipeInfoPanel = new JPanel();
         recipeInfoPanel.setBackground(Color.decode("#DDE0FB"));
         recipeInfoPanel.setBorder(new EmptyBorder(12, 12, 12, 12));
-        Dimension recipeInfoPanelDimensions = new Dimension(700, 600);
+        Dimension recipeInfoPanelDimensions = new Dimension(500, 600);
         recipeInfoPanel.setPreferredSize(recipeInfoPanelDimensions);
         recipeInfoPanel.setMaximumSize(recipeInfoPanelDimensions);
 
@@ -74,9 +105,96 @@ public class MainWindow {
         BoxLayout recipeInfoPanelLayout = new BoxLayout(recipeInfoPanel, BoxLayout.Y_AXIS);
         recipeInfoPanel.setLayout(recipeInfoPanelLayout);
 
-        JLabel header = new JLabel("Chicken and Rice");
-        header.setFont(new Font("Dialog", Font.BOLD, 24));
-        recipeInfoPanel.add(header);
+        JLabel recipeTitleLabel = new JLabel("Chicken and Rice");
+        recipeTitleLabel.setFont(new Font("Dialog", Font.BOLD, 24));
+        recipeInfoPanel.add(recipeTitleLabel);
+        recipeInfoPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        // Creates a panel to align the ingredients and steps horizontally on the same line
+        JPanel ingredientsAndStepsPanel = new JPanel();
+        ingredientsAndStepsPanel.setAlignmentX(0);
+        Dimension ingredientsAndStepsPanelDimensions = new Dimension(500, 300);
+        ingredientsAndStepsPanel.setPreferredSize(ingredientsAndStepsPanelDimensions);
+        ingredientsAndStepsPanel.setMaximumSize(ingredientsAndStepsPanelDimensions);
+        ingredientsAndStepsPanel.setBackground(Color.decode("#DDE0FB"));
+
+        BoxLayout ingredientsAndStepsPanelLayout = new BoxLayout(ingredientsAndStepsPanel, BoxLayout.X_AXIS);
+        ingredientsAndStepsPanel.setLayout(ingredientsAndStepsPanelLayout);
+
+        // Creates the ingredients panel that holds the heading and list
+        JPanel ingredientsPanel = new JPanel();
+        ingredientsPanel.setAlignmentX(0);
+        Dimension ingredientsPanelDimensions = new Dimension(250, 300);
+        ingredientsPanel.setPreferredSize(ingredientsPanelDimensions);
+        ingredientsPanel.setMaximumSize(ingredientsPanelDimensions);
+        ingredientsPanel.setBackground(Color.decode("#DDE0FB"));
+
+        BoxLayout ingredientsPanelLayout = new BoxLayout(ingredientsPanel, BoxLayout.Y_AXIS);
+        ingredientsPanel.setLayout(ingredientsPanelLayout);
+
+        JLabel ingredientsLabel = new JLabel("Ingredients");
+        ingredientsLabel.setFont(new Font("Dialog", Font.BOLD, 18));
+        ingredientsPanel.add(ingredientsLabel);
+        ingredientsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JEditorPane ingredientsList = new JEditorPane();
+        ingredientsList.setBackground(Color.decode("#DDE0FB"));
+        ingredientsList.setContentType("text/html");
+        ingredientsList.setEditable(false);
+        ingredientsList.setText("<html><ul style='margin:0px 20px'><li>Chicken</li><li>Rice</li></ul></html>");
+        ingredientsList.setAlignmentX(0);
+        ingredientsPanel.add(ingredientsList);
+
+        ingredientsAndStepsPanel.add(ingredientsPanel);
+
+        // Creates the steps panel that holds the heading and list
+        JPanel stepsPanel = new JPanel();
+        stepsPanel.setAlignmentX(0);
+        Dimension stepsPanelDimensions = new Dimension(250, 300);
+        stepsPanel.setPreferredSize(stepsPanelDimensions);
+        stepsPanel.setMaximumSize(stepsPanelDimensions);
+        stepsPanel.setBackground(Color.decode("#DDE0FB"));
+
+        BoxLayout stepsPanelLayout = new BoxLayout(stepsPanel, BoxLayout.Y_AXIS);
+        stepsPanel.setLayout(stepsPanelLayout);
+
+        JLabel stepsLabel = new JLabel("Steps");
+        stepsLabel.setFont(new Font("Dialog", Font.BOLD, 18));
+        stepsPanel.add(stepsLabel);
+        stepsPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JEditorPane stepsList = new JEditorPane();
+        stepsList.setBackground(Color.decode("#DDE0FB"));
+        stepsList.setContentType("text/html");
+        stepsList.setEditable(false);
+        stepsList.setText("<html><ol style='margin:0px 20px'><li>Cook chicken</li><li>Cook rice</li></ol></html>");
+        stepsList.setAlignmentX(0);
+        stepsPanel.add(stepsList);
+
+        ingredientsAndStepsPanel.add(stepsPanel);
+
+        recipeInfoPanel.add(ingredientsAndStepsPanel);
+
+        recipeInfoPanel.add(Box.createVerticalGlue());
+
+        // Creates a panel to horizontally align the edit and delete buttons
+        JPanel recipeInfoButtonsPanel = new JPanel();
+        recipeInfoButtonsPanel.setAlignmentX(0);
+        Dimension recipeInfoButtonsPanelDimensions = new Dimension(500, 50);
+        recipeInfoButtonsPanel.setPreferredSize(recipeInfoButtonsPanelDimensions);
+        recipeInfoButtonsPanel.setMaximumSize(recipeInfoButtonsPanelDimensions);
+        recipeInfoButtonsPanel.setBackground(Color.decode("#DDE0FB"));
+
+        BoxLayout recipeInfoButtonsPanelLayout = new BoxLayout(recipeInfoButtonsPanel, BoxLayout.X_AXIS);
+        recipeInfoButtonsPanel.setLayout(recipeInfoButtonsPanelLayout);
+
+        JButton editRecipeButton = new Button("Edit Recipe");
+        recipeInfoButtonsPanel.add(editRecipeButton);
+        recipeInfoButtonsPanel.add(Box.createRigidArea(new Dimension(20, 0)));
+        JButton deleteRecipeButton = new Button("Delete Recipe");
+        recipeInfoButtonsPanel.add(deleteRecipeButton);
+
+        recipeInfoPanel.add(recipeInfoButtonsPanel);
 
         this.baseLayoutPanel.add(recipeInfoPanel);
     }
--- RecipeBook/src/PopupWindow.java
+++ RecipeBook/src/PopupWindow.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class PopupWindow {
@@ -14,10 +15,147 @@ public class PopupWindow {
         this.baseLayoutPanel.setLayout(baseLayout);
         this.baseLayoutPanel.setBackground(Color.decode("#DDE0FB"));
 
+        this.buildLeftFormPanel();
+        this.buildRightFormPanel();
+
         this.baseFrame.getContentPane().add(this.baseLayoutPanel);
-        this.baseFrame.setPreferredSize(new Dimension(800, 500));
+        this.baseFrame.setPreferredSize(new Dimension(600, 500));
         this.baseFrame.pack();
+        this.baseFrame.setResizable(false);
         this.baseFrame.setVisible(false);
+    }
+    // Creates the left side of the form
+    public void buildLeftFormPanel(){
+        JPanel leftFormPanel = new JPanel();
+        Dimension leftFormPanelDimensions = new Dimension(300,500);
+        leftFormPanel.setPreferredSize(leftFormPanelDimensions);
+        leftFormPanel.setMaximumSize(leftFormPanelDimensions);
+        leftFormPanel.setBorder(new EmptyBorder(12, 12, 12, 12));
+        leftFormPanel.setBackground(Color.decode("#DDE0FB"));
+
+        // Sets a vertical layout
+        BoxLayout leftFormPanelLayout = new BoxLayout(leftFormPanel, BoxLayout.Y_AXIS);
+        leftFormPanel.setLayout(leftFormPanelLayout);
+
+        JLabel recipeNameLabel = new JLabel("Recipe Name");
+        leftFormPanel.add(recipeNameLabel);
+        leftFormPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        JTextField recipeNameInput = new JTextField();
+        recipeNameInput.setAlignmentX(0);
+        Dimension recipeNameInputDimensions = new Dimension(300,24);
+        recipeNameInput.setPreferredSize(recipeNameInputDimensions);
+        recipeNameInput.setMaximumSize(recipeNameInputDimensions);
+        leftFormPanel.add(recipeNameInput);
+        leftFormPanel.add(Box.createRigidArea(new Dimension(0, 16)));
+
+        JLabel ingredientsLabel = new JLabel("Ingredients");
+        leftFormPanel.add(ingredientsLabel);
+        leftFormPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        DefaultListModel<String> ingredientsModel = new DefaultListModel<>();
+        ingredientsModel.addElement("Bacon");
+        ingredientsModel.addElement("Egg");
+        ingredientsModel.addElement("Cheese");
+
+        JList<String> ingredientsList = new JList<>(ingredientsModel);
+        ingredientsList.setAlignmentX(0);
+        Dimension ingredientsListDimensions = new Dimension(300,250);
+        ingredientsList.setPreferredSize(ingredientsListDimensions);
+        ingredientsList.setMaximumSize(ingredientsListDimensions);
+        leftFormPanel.add(ingredientsList);
+
+        JPanel ingredientButtonsPanel = new JPanel();
+        ingredientButtonsPanel.setBorder(new EmptyBorder(12, 0, 12, 0));
+        ingredientButtonsPanel.setBackground(Color.decode("#DDE0FB"));
+        ingredientButtonsPanel.setAlignmentX(0);
+        // Sets a horizontal layout
+        BoxLayout ingredientButtonsPanelLayout = new BoxLayout(ingredientButtonsPanel, BoxLayout.X_AXIS);
+        ingredientButtonsPanel.setLayout(ingredientButtonsPanelLayout);
+
+        Dimension buttonDimensions = new Dimension(140, 25);
+
+        JButton addNewIngredientButton = new Button("Add");
+        addNewIngredientButton.setFont(new Font("Dialog", Font.PLAIN, 14));
+        addNewIngredientButton.setPreferredSize(buttonDimensions);
+        addNewIngredientButton.setMaximumSize(buttonDimensions);
+        ingredientButtonsPanel.add(addNewIngredientButton);
+
+        ingredientButtonsPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+
+        JButton deleteIngredientButton = new Button("Delete");
+        deleteIngredientButton.setFont(new Font("Dialog", Font.PLAIN, 14));
+        deleteIngredientButton.setPreferredSize(buttonDimensions);
+        deleteIngredientButton.setMaximumSize(buttonDimensions);
+        ingredientButtonsPanel.add(deleteIngredientButton);
+
+        leftFormPanel.add(ingredientButtonsPanel);
+
+        // Aligns button at bottom
+        leftFormPanel.add(Box.createVerticalGlue());
+        JButton addNewRecipeButton = new Button("Add New Recipe");
+        leftFormPanel.add(addNewRecipeButton);
+
+
+        this.baseLayoutPanel.add(leftFormPanel);
+    }
+    // Creates the right side of the form
+    public void buildRightFormPanel(){
+        JPanel rightFormPanel = new JPanel();
+        Dimension rightFormPanelDimensions = new Dimension(300, 500);
+        rightFormPanel.setPreferredSize(rightFormPanelDimensions);
+        rightFormPanel.setMaximumSize(rightFormPanelDimensions);
+        rightFormPanel.setBorder(new EmptyBorder(12, 12, 12, 12));
+        rightFormPanel.setBackground(Color.decode("#DDE0FB"));
+
+        // Sets a vertical layout
+        BoxLayout rightFormPanelLayout = new BoxLayout(rightFormPanel, BoxLayout.Y_AXIS);
+        rightFormPanel.setLayout(rightFormPanelLayout);
+
+        rightFormPanel.add(Box.createRigidArea(new Dimension(0, 67)));
+
+        JLabel stepsLabel = new JLabel("Steps");
+        rightFormPanel.add(stepsLabel);
+        rightFormPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        DefaultListModel<String> stepsModel = new DefaultListModel<>();
+        stepsModel.addElement("Add customer to grill");
+
+        JList<String> stepsList = new JList<>(stepsModel);
+        Dimension stepsListDimensions = new Dimension(300,250);
+        stepsList.setPreferredSize(stepsListDimensions);
+        stepsList.setMaximumSize(stepsListDimensions);
+        stepsList.setAlignmentX(0);
+        rightFormPanel.add(stepsList);
+
+        JPanel stepsButtonsPanel = new JPanel();
+        stepsButtonsPanel.setBorder(new EmptyBorder(12, 0, 12, 0));
+        stepsButtonsPanel.setBackground(Color.decode("#DDE0FB"));
+        stepsButtonsPanel.setAlignmentX(0);
+        // Sets a horizontal layout
+        BoxLayout stepsButtonsPanelLayout = new BoxLayout(stepsButtonsPanel, BoxLayout.X_AXIS);
+        stepsButtonsPanel.setLayout(stepsButtonsPanelLayout);
+
+        Dimension buttonDimensions = new Dimension(140, 25);
+
+        JButton addNewStepButton = new Button("Add");
+        addNewStepButton.setFont(new Font("Dialog", Font.PLAIN, 14));
+        addNewStepButton.setPreferredSize(buttonDimensions);
+        addNewStepButton.setMaximumSize(buttonDimensions);
+        stepsButtonsPanel.add(addNewStepButton);
+
+        stepsButtonsPanel.add(Box.createRigidArea(new Dimension(5, 0)));
+
+        JButton deleteStepButton = new Button("Delete");
+        deleteStepButton.setFont(new Font("Dialog", Font.PLAIN, 14));
+        deleteStepButton.setPreferredSize(buttonDimensions);
+        deleteStepButton.setMaximumSize(buttonDimensions);
+        stepsButtonsPanel.add(deleteStepButton);
+
+        rightFormPanel.add(stepsButtonsPanel);
+        rightFormPanel.add(Box.createVerticalGlue());
+
+        this.baseLayoutPanel.add(rightFormPanel);
     }
     public void show(boolean shouldShow){
         this.baseFrame.setVisible(shouldShow);
