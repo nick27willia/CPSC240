32021b880bb7ffd30afd3fdf5baef4a94e6886f4
Nate <nate@2bttns.com>
2022-04-18 10:35:36 -0400

    Test out recipeManager

--- RecipeBook/src/Main.java
+++ RecipeBook/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        PopupWindow popupWindow = new PopupWindow();
-        MainWindow mainWindow = new MainWindow(popupWindow);
+        RecipeManager recipeManager = new RecipeManager();
+//        PopupWindow popupWindow = new PopupWindow();
+//        MainWindow mainWindow = new MainWindow(popupWindow);
     }
 }
